f686cf7ff676b83d63745f751057d5dcb6683db0
veto2922 <veto2922@gmail.com>
2024-07-01 20:36:37 -0700

    Uploaded file to the system

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,7 +1,9 @@
 from .BaseController import BaseController
 from fastapi import  UploadFile
 from models import ResponseSignal
-
+from .ProjectController import ProjectController
+import re
+import os
 
 class DataController(BaseController):
     
@@ -18,3 +20,37 @@ class DataController(BaseController):
             return False , ResponseSignal.FILE_SIZE_EXCEEDED.value
         
         return True , ResponseSignal.FILE_UPLOAD_SUCCESS.value
+    
+    def generate_uniqu_filename(self , original_file_name:str , project_id :str):
+        
+        random_key = self.generate_random_string()
+        project_path = ProjectController().get_project_path(project_id=project_id)
+        
+        cleane_file_name = self.get_clean_file_name(
+            orig_file_name= original_file_name
+        )
+        
+        new_file_path = os.path.join(
+            project_path,
+            random_key + '_' + cleane_file_name
+        )
+        
+        while os.path.exists(new_file_path):
+            random_key = self.generate_random_string()
+            new_file_path = os.path.join
+            (
+                 project_path,
+                random_key + '_' + cleane_file_name
+            )
+        
+        return new_file_path
+        
+    def get_clean_file_name(self, orig_file_name: str):
+
+        # remove any special characters, except underscore and .
+        cleaned_file_name = re.sub(r'[^\w.]', '', orig_file_name.strip())
+
+        # replace spaces with underscore
+        cleaned_file_name = cleaned_file_name.replace(" ", "_")
+
+        return cleaned_file_name
--- src/routes/data.py
+++ src/routes/data.py
@@ -3,9 +3,12 @@ from fastapi.responses import JSONResponse
 import os
 
 from helpers.config import get_settings , Settings
-from controllers import DataController
+from controllers import DataController , ProjectController
+import aiofiles
+from models import ResponseSignal
+import logging 
 
-
+logger = logging.getLogger('uvicorn.error')
 
 data_router = APIRouter(
     prefix = '/api/v1/data',
@@ -26,7 +29,31 @@ async def upload_data(project_id :str , file : UploadFile,
             content = { 'signal' : result_signal}
         )
         
+        
+    project_dir_path = ProjectController().get_project_path(project_id= project_id)
     
+    file_path = data_controller.generate_uniqu_filename(
+        original_file_name=file.filename , 
+        project_id= project_id
+    )
+    
+    try:
+        async with aiofiles.open(file_path , 'wb') as f:
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+            
+    except Exception as e:
+        logger.error(f'Error while uploading file : {e}')
+        
+        return JSONResponse(
+            status_code = status.HTTP_400_BAD_REQUEST,
+            content = { 'signal' : ResponseSignal.FILE_UPLOAD_FAILED.value}
+        )
+        
+    return JSONResponse(
+            content = { 'signal' : ResponseSignal.FILE_UPLOAD_SUCCESS.value}
+        )
+
     
     
      
